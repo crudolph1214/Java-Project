4d0521fe14cba6c27c9ce0d7d6d947d05dc684f4
Clay Rudolph <33466352+crudolph1214@users.noreply.github.com>
2018-05-10 10:47:41 -0500

    Create MainApp.java

---
+++ src/MainApp.java
@@ -0,0 +1,15 @@
+// Class Name:        Reservation System
+// Language:          Java
+// Author:            Clay Rudolph
+// Date Written:      6/6/2017 - 6/8/2017
+// Application Area:  IS Project
+
+//Runs the program
+public class MainApp
+{
+    public static void main(String[] args)
+    {
+        new ReservationSystem();
+    }
+
+}
